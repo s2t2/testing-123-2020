e2ecec2af73c50f83417072bff5cff612ca15945
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-23 17:40:55 -0400

    Configured basic function with test

---
+++ my_script.py
@@ -0,0 +1,19 @@
+
+# testing-123/my_script.py
+
+def enlarge(i):
+    return i * 100
+
+# need to remove from global scope in order to import other things from this script
+#my_number = float(input("Please input a number: "))
+##n = enlarge(8)
+#n = enlarge(my_number)
+#print("ENLARGING THE NUMBER:", n)
+
+if __name__ == "__main__":
+    # only run this if invoked from command-line
+    # not if imported by another file
+    my_number = float(input("Please input a number: "))
+    #n = enlarge(8)
+    n = enlarge(my_number)
+    print("ENLARGING THE NUMBER:", n)
---
+++ my_test.py
@@ -0,0 +1,8 @@
+
+# testing-123/my_test.py
+
+from my_script import enlarge
+
+def test_enlarge():
+    result = enlarge(3)
+    assert result == 300
