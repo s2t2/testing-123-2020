e3ae56f1ad531cd1806883eea0f1fb8caf28b6b5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-25 18:27:46 -0400

    Test code climate again, after integrating with the right repo this time

--- app/new_feature.py
+++ app/new_feature.py
@@ -11,3 +11,17 @@ def some_duplicative_code():
 
     x = [1,2,3]
     print(x)
+
+
+def high_complexity():
+
+    if 2 == 2:
+        if 5 == 5:
+            if 6 == 6:
+                if 9 == 9:
+                    if 10 == 10:
+                        if 11 == 11:
+                            if 12 == 12:
+                                if 13 == 13:
+                                    if 14 == 14:
+                                        print("COMPLEX")
